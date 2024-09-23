315c01145beff1b6e3eee416b1bfaeda2f01b615
Daniel Schor <daniel.schor@gmx.de>
2024-09-23 21:49:44 +0200

    Extract event responses to functions

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,128 @@ impl TypeMapKey for VoteHandler {
     type Value = VoteHandler;
 }
 
+async fn votekick_init(ctx: &Context, msg: &Message) {
+    let args: Vec<&str> = msg.content.split_whitespace().collect();
+
+    if args.len() < 2 || msg.mentions.is_empty() {
+        if let Err(why) = msg
+            .channel_id
+            .say(&ctx.http, "Please add mention user and retry.")
+            .await
+        {
+            println!("Error sending message: {:?}", why);
+        }
+        return;
+    }
+
+    if let Ok(votekick_message) = msg
+        .channel_id
+        .say(
+            &ctx.http,
+            format!(
+                "Timeout: <@{}> for 1 min.\n Please vote with a ✅ on this message.",
+                msg.mentions[0].id
+            ),
+        )
+        .await
+    {
+        let msg_id = votekick_message.id;
+
+        // Begin accessing the data map
+        let mut data = ctx.data.write().await;
+        if let Some(vote_handler) = data.get_mut::<VoteHandler>() {
+            vote_handler
+                .vote_counts
+                .insert(msg_id.get(), (msg.mentions[0].id.get(), 0));
+        } else {
+            println!("Error: VoteHandler not found in TypeMap.");
+        }
+    }
+}
+
+async fn votekick_vote(ctx: &Context, reaction: &Reaction) {
+    if reaction.emoji == ReactionType::Unicode("✅".to_string()) {
+        // get vote_handler from ctx.data
+        // Begin accessing the data map
+        let mut data = ctx.data.write().await;
+        if let Some(vote_handler) = data.get_mut::<VoteHandler>() {
+            let msg_id = reaction.message_id.get();
+            // if message is not a vote -> return
+            if !vote_handler.vote_counts.contains_key(&msg_id) {
+                println!(
+                    "Not a vote message. {:?} {}",
+                    vote_handler.vote_counts, &msg_id
+                );
+                return;
+            }
+            let user_id = vote_handler.vote_counts.get(&msg_id).unwrap().0;
+            let mut count = vote_handler.vote_counts.get_mut(&msg_id).unwrap().1;
+            count += 1;
+
+            if count >= 1 {
+                vote_handler.vote_counts.insert(msg_id, (user_id, 0));
+
+                // Get the guild ID and fetch the member
+                if let Some(guild_id) = reaction.guild_id {
+                    // Fetch the member object for the user
+                    if let Ok(mut member) = guild_id.member(&ctx.http, user_id).await {
+                        // Set the timeout duration (1 minute from now)
+                        let timeout_duration = chrono::Utc::now() + chrono::Duration::minutes(1);
+
+                        // Convert to serenity's Timestamp
+                        let timeout_timestamp: serenity::model::Timestamp = timeout_duration.into();
+
+                        // Disable communication for the specified duration
+                        if let Err(why) = member
+                            .disable_communication_until_datetime(&ctx.http, timeout_timestamp)
+                            .await
+                        {
+                            println!("Error muting user: {:?}", why);
+                        } else {
+                            println!(
+                                "{}: User <@{}> has been muted for 1 minute.",
+                                date_helper::timestamp_string(),
+                                user_id
+                            );
+
+                            // Notify the channel that the user has been muted
+                            if let Err(why) = reaction
+                                .channel_id
+                                .say(
+                                    &ctx.http,
+                                    format!("<@{}> has been muted for 1 minute.", user_id),
+                                )
+                                .await
+                            {
+                                println!("Error sending message: {:?}", why);
+                            }
+                        }
+                    } else {
+                        println!("Could not find member in the guild.");
+                    }
+                } else {
+                    println!("Reaction did not occur in a guild.");
+                }
+            } else {
+                vote_handler.vote_counts.insert(msg_id, (user_id, count));
+            }
+        } else {
+            println!("Error: VoteHandler not found in TypeMap.");
+        }
+    }
+}
+
+async fn init_vote_handler(data: &mut TypeMap) {
+    data.insert::<VoteHandler>(VoteHandler {
+        vote_counts: HashMap::new(),
+    });
+}
+
+async fn init_type_map(data: &mut TypeMap) {
+    init_vote_handler(data).await;
+    // Add other handlers here
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     // Message sent (anywhere) event
@@ -39,40 +161,10 @@ impl EventHandler for Handler {
         }
         // Votekick command
         else if msg.content.starts_with("!timeout") {
-            let args: Vec<&str> = msg.content.split_whitespace().collect();
-
-            if args.len() < 2 || msg.mentions.is_empty() {
-                if let Err(why) = msg
-                    .channel_id
-                    .say(&ctx.http, "Please add mention user and retry.")
-                    .await
-                {
-                    println!("Error sending message: {:?}", why);
-                }
-                return;
-            }
-
-            if let Ok(votekick_message) = msg
-                .channel_id
-                .say(
-                    &ctx.http,
-                    format!("Timeout: <@{}> for 1 min.\n Please vote with a ✅ on this message.", msg.mentions[0].id),
-                )
-                .await
-            {
-                let msg_id = votekick_message.id;
-
-                // Begin accessing the data map
-                let mut data = ctx.data.write().await;
-                if let Some(vote_handler) = data.get_mut::<VoteHandler>() {
-                    vote_handler
-                        .vote_counts
-                        .insert(msg_id.get(), (msg.mentions[0].id.get(), 0));
-                } else {
-                    println!("Error: VoteHandler not found in TypeMap.");
-                }
-            }
+            votekick_init(&ctx, &msg).await;
         }
+        // Other interactive commands here
+        // else if (msg.content....) { }
     }
 
     // Bot (self) joins server event
@@ -83,10 +175,9 @@ impl EventHandler for Handler {
             ready.user.name
         );
 
+        // Initialize the VoteHandler in the TypeMap ("Bot chache")
         let mut data = ctx.data.write().await;
-        data.insert::<VoteHandler>(VoteHandler {
-            vote_counts: HashMap::new(),
-        });
+        init_type_map(&mut data).await;
     }
 
     // Reaction added event
@@ -101,63 +192,10 @@ impl EventHandler for Handler {
             reaction.message_id.get()
         );
 
-        if reaction.emoji == ReactionType::Unicode("✅".to_string()) {
-            // get vote_handler from ctx.data
-            // Begin accessing the data map
-            let mut data = ctx.data.write().await;
-            if let Some(vote_handler) = data.get_mut::<VoteHandler>() {
-                let msg_id = reaction.message_id.get();
-                // if message is not a vote -> return
-                if !vote_handler.vote_counts.contains_key(&msg_id) {
-                    println!("Not a vote message. {:?} {}", vote_handler.vote_counts, &msg_id);
-                    return;
-                }
-                let user_id = vote_handler.vote_counts.get(&msg_id).unwrap().0;
-                let mut count = vote_handler.vote_counts.get_mut(&msg_id).unwrap().1;
-                count += 1;
-
-                if count >= 1 {
-                    vote_handler.vote_counts.insert(msg_id, (user_id, 0));
-                
-                    // Get the guild ID and fetch the member
-                    if let Some(guild_id) = reaction.guild_id {
-                        // Fetch the member object for the user
-                        if let Ok(mut member) = guild_id.member(&ctx.http, user_id).await {
-                            // Set the timeout duration (1 minute from now)
-                            let timeout_duration = chrono::Utc::now() + chrono::Duration::minutes(1);
-                            
-                            // Convert to serenity's Timestamp
-                            let timeout_timestamp: serenity::model::Timestamp = timeout_duration.into();
-                
-                            // Disable communication for the specified duration
-                            if let Err(why) = member.disable_communication_until_datetime(&ctx.http, timeout_timestamp).await {
-                                println!("Error muting user: {:?}", why);
-                            } else {
-                                println!("{}: User <@{}> has been muted for 1 minute.", date_helper::timestamp_string(), user_id);
-                
-                                // Notify the channel that the user has been muted
-                                if let Err(why) = reaction.channel_id.say(&ctx.http, format!("<@{}> has been muted for 1 minute.", user_id)).await {
-                                    println!("Error sending message: {:?}", why);
-                                }
-                            }
-                        } else {
-                            println!("Could not find member in the guild.");
-                        }
-                    } else {
-                        println!("Reaction did not occur in a guild.");
-                    }
-                } else {
-                    vote_handler.vote_counts.insert(msg_id, (user_id, count));
-                }
-                
-                
-            } else {
-                println!("Error: VoteHandler not found in TypeMap.");
-            }
-            // -----------
-        }
+        votekick_vote(&ctx, &reaction).await;
     }
 
+    // ! if there are more uses for this event: extract user time tracking to a separate function
     // Joined a voice channel event
     async fn voice_state_update(&self, _ctx: Context, old: Option<VoiceState>, new: VoiceState) {
         // load json to hashmap
